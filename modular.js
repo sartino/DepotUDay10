ee8d199cf10b3b0aac5f918daa7851d5797ec008
Steve Sartino <stevesartino@yahoo.com>
2015-09-26 19:45:28 -0500

    Added 2 more.

--- modular.js
+++ modular.js
@@ -1,3 +1,33 @@
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function (dir, filterStr, callback) {
+
+  fs.readdir(dir, function (err, list) {
+    if (err)
+      return callback(err)
+
+    list = list.filter(function (file) {
+      return path.extname(file) === '.' + filterStr
+    })
+
+    callback(null, list)
+  })
+}
+var filterFn = require('./solution_filter.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filterFn(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
+
+/*
 create 2 files.
 prints a list of files filtered by WindowTimersExtension
 1st arg dir name
@@ -10,25 +40,7 @@ if an error, 1st arg will be null
 2nd will be the data 
 data will be list of files as array 
 
-/*
-var fs = require('fs')
-var path = require('path')
-
-    fs.readdir(process.argv[2], function (err, list) {
-      list.forEach(function (file) {
-        if (path.extname(file) === '.' + process.argv[3])
-          console.log(file)
-      })
-    })
-var fs = require('fs');
-var path = require('path');
-
-fs.readdir(process.argv[2], function callback(err, list) {
-	for(var i=0; i < list.length; i++) {
-		if(list[i].match('.md'))
-		console.log(list[i]);
-	}
-})    <----offical answer---->
+    <----offical answer---->
 var fs = require('fs')
     var path = require('path')
 
